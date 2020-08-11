3a0f1213dc680bc8aa8e3feba86e8cc1ac1b54be
donoaga <donoaga.radu@gmail.com>
2020-08-11 18:31:57 +0300

    expand height of images on  <640px

--- src/section.vue
+++ src/section.vue
@@ -517,6 +517,20 @@
 
     }
 
+    @media only screen and (max-width: 1100px) {
+        .section-hello-world {
+            &::v-deep {
+
+                .content-container {
+
+                    .left-container {
+                        margin-top: 60px
+                    }
+                }
+            }
+        }
+    }
+
     @media only screen and (max-width: 768px) {
         .section-hello-world {
             &::v-deep {
@@ -583,7 +597,7 @@
                     }
 
                     .right-container {
-                        height: 300px;
+                        height: 340px;
                     }
                 }
             }
